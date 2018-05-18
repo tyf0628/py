a2a58e93e8aeda1e16a8a1705756f1e0177ba89f
tyf0628 <379229822@qq.com>
2018-05-18 14:33:43 +0800

    删除：      cpu_avg.sh 	删除：      host.py 	删除：      weixin.py

--- cpu_avg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dir=/usr/local/zabbix/share/zabbix/externalscripts/cpu
-mkdir -p $dir/$1
-cat /usr/local/zabbix/share/zabbix/externalscripts/cpu_temp.sql | sed -e "s/hostname_ip/$1/g" > $dir/$1/cpu_avg_new.sql
-mysql --defaults-extra-file=/etc/no_warning_mysql.cnf < $dir/$1/cpu_avg_new.sql | awk '{ sum += $4; } END {print sum/(NR-1)}'
-
